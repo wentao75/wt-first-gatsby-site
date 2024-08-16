eba5c2cf04dddb377efa7e91c2147c08bd506af3
wentao75 <wentao75@gmail.com>
2024-08-16 16:48:04 +0800

    Gatsby教学3完成！

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,15 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+// const adapter = require("gatsby-adapter-netlify");
+
 module.exports = {
-  siteMetadata: {
-    title: `WT First Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [],
-}
+    siteMetadata: {
+        title: `WT First Gatsby Site`,
+        siteUrl: `https://www.yourdomain.tld`,
+    },
+    // adapter: adapter({
+    //     excludeDatastoreFromEngineFunction: false,
+    // }),
+    plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import * as React from "react";
-import { Link } from "gatsby";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
     return (
         <Layout pageTitle="首页">
             <p>我正在Gatsby教学学习</p>
+            <StaticImage
+                alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+                src="../images/E1oMV3QVgAIr1NT.jpeg"
+            />
         </Layout>
     );
 };
