6e79b0039bf8eb79f1ea6aafd4956cf3cfe8a0b3
wentao75 <wentao75@gmail.com>
2024-08-19 17:07:04 +0800

    完成Gatsby教学第六章

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,14 +1,18 @@
 import * as React from "react";
-import { graphql } from "gatsby";
-import Layout from "../components/layout.js";
-import Seo from "../components/seo";
+import { Link, graphql } from "gatsby";
+import Layout from "../../components/layout.js";
+import Seo from "../../components/seo";
 
 const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="我的博客">
             {data.allMdx.nodes.map((node) => (
                 <article key={node.id}>
-                    <h2>{node.frontmatter.title}</h2>
+                    <h2>
+                        <Link to={`/blog/${node.frontmatter.slug}`}>
+                            {node.frontmatter.title}
+                        </Link>
+                    </h2>
                     <p>发布于：{node.frontmatter.date}</p>
                     <p>{node.excerpt}</p>
                 </article>
@@ -24,9 +28,9 @@ export const query = graphql`
                 frontmatter {
                     date(formatString: "YYYY年MM月DD日")
                     title
+                    slug
                 }
                 id
-                excerpt
             }
         }
     }
